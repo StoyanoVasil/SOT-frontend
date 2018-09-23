5d7c0ecb58688ecbcb756105fb5294aeba5d740b
i333656 <vasil.stoyanov@feg.bg>
2018-09-23 18:27:24 +0200

    add login and register pages

--- src/views.py
+++ src/views.py
@@ -4,3 +4,13 @@ from src import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
